5655b5eb51128d5b8b8fc0aea7523a8f8cc65518
didtlahs <didtlahs>
2026-07-16 14:39:55 +0900

    print time stamp

--- cmd_seq.cpp
+++ cmd_seq.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <unistd.h>
-
+#include <ctime>
+#include <chrono>
 int main() {
     std::vector<std::string> cmds = {
         "Move Forward",
@@ -13,6 +14,8 @@ int main() {
     };
 
     for (const std::string& cmd : cmds) {
+	auto now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+	std::cout << std::ctime(&now);
         std::cout << "Robot performs: " << cmd << std::endl;
         sleep(1);  // 1초 대기
     }
